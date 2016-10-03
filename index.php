d6aeeb3751110a1ce7c1b1cad8e8f17544a44da6
FernetteDev <drecks.fernette@gmail.com>
2016-10-03 17:39:57 +0200

    ajout de la class select et ajout dans le readme

--- index.php
+++ index.php
@@ -7,25 +7,6 @@
 	$head->script(array('jquery_3.1.1', 'script'));
 	$head->afficher_head();
 
-	$balise = new \Html\balise();
-	$balise->a('toto');
-
-	$mdp = new \Securite\Session();
-
-
-	$md5 = sha1('toto');
-	var_dump($md5);
-	$md5 = md5('toto');
-	var_dump($md5);
-
-	var_dump($mdp->crypter('toto'));
-	var_dump(mcrypt_encrypt());
-	var_dump(MCRYPT_RIJNDAEL_256);
-	var_dump(MCRYPT_MODE_ECB);
-
-	var_dump(mcrypt_list_algorithms());
-	var_dump(mcrypt_list_modes());
-
 	?>
 
 
